1712de4ec2db36d840194ac9243933b64f4d5259
LAB1_14 <williamkelvin@alu.ufc.br>
2023-06-29 17:12:11 -0300

    Inserindo arquivos

---
+++ frontend/src/components/Artigos.vue
@@ -0,0 +1,102 @@
+<template>
+    <div class="card">
+        <div class="post"
+            v-for="artigo in artigos"
+            v-bind:key="artigo.id">
+            <h3> {{ artigo.content }}</h3>
+            <p></p>
+        </div>
+    </div>
+</template>
+
+<script>
+import api from "../service/api"
+
+export default{
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Artigos',
+    data(){
+        return{
+            artigos: [],
+            artigo:{
+                id:'',
+                content:'',
+            },
+            editar: false
+            }
+        },
+        methods: {
+            obterArtigos(){
+                api.get("posts")
+                .then(res => {
+                    this.artigos = res.data.data;
+                })
+                .catch(err => console.log(err))
+            }
+            },
+            created(){
+                this.obterArtigos()
+            }
+        }
+    
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+ .card{
+   background-color: #fff;
+   color: #333;
+ }
+.hello{
+  color: #333;
+  padding: 20px;
+  max-width: 1200px;
+  margin: auto;
+}
+ .post{
+   border:1px solid silver;
+   padding: 10px;
+   margin-top: 10px;
+   text-align: left;
+ }
+
+ .danger{
+   color: #fff;
+   background: red;
+   padding: 5px;
+   margin-top: 5px;
+ }
+
+.info{
+  color: #fff;
+  background: blue;
+  padding: 5px;
+  margin-top: 5px;
+}
+.success{
+  color: #fff;
+  background: green;
+  padding: 15px;
+  border: 1px solid #333;
+}
+.primary{
+  color: #333;
+  background: cyan;
+  padding: 15px;
+  border: 1px solid #333;
+}
+</style>
---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="navbar">
+        <h1>
+            <a href="./">
+                Construindo minha API com Vue.js
+            </a>
+        </h1>
+    </div>
+</template>
+
+<script>
+export default{
+    name:'NavBar'
+}
+</script>
+
+<style>
+.navebar{
+    background: #282c34;
+    color: #abb2bf;
+    padding: 10px;
+}
+.navebar a{
+    color: #abb2bf;
+    text-decoration: none;
+}
+
+</style>
